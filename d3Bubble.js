9585a76adc23ef32e51985e704da222ccfaaf63f
Chase Johnston <46426781+blueberrycola@users.noreply.github.com>
2021-06-23 19:54:38 -0400

    Add files via upload

--- d3Bubble.js
+++ d3Bubble.js
@@ -27,8 +27,9 @@ let dates = interval.range(
 );
 
 interval = d3.utcMonth // interval between animation frames
-//Parses data from nations.json file
-let data = (await FileAttachment("nations.json").json())
+var json = FileAttachment("nations.json").json();
+//Parses data from nations.json file NOTE: FileAttachment is used by observable library
+data = (json)
   .map(({name, region, income, population, lifeExpectancy}) => ({
     name,
     region,
@@ -36,7 +37,7 @@ let data = (await FileAttachment("nations.json").json())
     population: parseSeries(population),
     lifeExpectancy: parseSeries(lifeExpectancy)
 }));
-
+//???
 function valueAt(values, date) {
     const i = bisectDate(values, date, 0, values.length - 1);
     const a = values[i];
@@ -46,7 +47,69 @@ function valueAt(values, date) {
       return a[1] * (1 - t) + b[1] * t;
     }
     return a[1];
-  }
+}
+//???
+function dataAt(date) {
+    return data.map(d => ({
+      name: d.name,
+      region: d.region,
+      income: valueAt(d.income, date),
+      population: valueAt(d.population, date),
+      lifeExpectancy: valueAt(d.lifeExpectancy, date)
+    }));
+}
+//define grid for plot
+let grid = g => g
+    .attr("stroke", "currentColor")
+    .attr("stroke-opacity", 0.1)
+    .call(g => g.append("g")
+      .selectAll("line")
+      .data(x.ticks())
+      .join("line")
+        .attr("x1", d => 0.5 + x(d))
+        .attr("x2", d => 0.5 + x(d))
+        .attr("y1", margin.top)
+        .attr("y2", height - margin.bottom))
+    .call(g => g.append("g")
+      .selectAll("line")
+      .data(y.ticks())
+      .join("line")
+        .attr("y1", d => 0.5 + y(d))
+        .attr("y2", d => 0.5 + y(d))
+        .attr("x1", margin.left)
+        .attr("x2", width - margin.right));
+//Define yAxis on plot graph
+let yAxis = g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select(".domain").remove())
+    .call(g => g.append("text")
+        .attr("x", -margin.left)
+        .attr("y", 10)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text("↑ Life expectancy (years)"))
+//Define xAxis on plot graph
+let xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).ticks(width / 80, ","))
+    .call(g => g.select(".domain").remove())
+    .call(g => g.append("text")
+        .attr("x", width)
+        .attr("y", margin.bottom - 4)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "end")
+        .text("Income per capita (dollars) →"))
+//Vars for circle state???
+var color = d3.scaleOrdinal(data.map(d => d.region), d3.schemeCategory10).unknown("black");
+var radius = d3.scaleSqrt([0, 5e8], [0, width / 24]);
+var y = d3.scaleLinear([14, 86], [height - margin.bottom, margin.top])
+var x = d3.scaleLog([200, 1e5], [margin.left, width - margin.right]);
+var currentData = dataAt(date)
+
+update = chart.update(currentData);
+
+
 
 
 
